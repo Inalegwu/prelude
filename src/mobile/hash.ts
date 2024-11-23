c7bb1e5cae9b0b3583c29623113305bd0a459b78
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-11-23 19:09:43 +0000

    version bump

--- src/mobile/hash.ts
+++ src/mobile/hash.ts
@@ -1,6 +1,21 @@
 // deno-lint-ignore-file no-namespace
 
-// Hashing functions suitable for a React Native environment
+/**
+ * A namespace containing utility functions for generating random hash strings
+ * and UUID-like strings, suitable for use in a React Native environment.
+ *
+ * @namespace Hash
+ *
+ * @example
+ * // Example usage of the Hash namespace:
+ * import { Hash } from '@disgruntleddevs/prelude/mobile';
+ *
+ * const shortHash = Hash.randomXDigitHash(4);
+ * console.log(shortHash); // Outputs a random 4-character hexadecimal string
+ *
+ * const customUuid = Hash.randomuuid("user", "-", 5);
+ * console.log(customUuid); // Outputs something like "user-a3f9b-c1e34-9da84"
+ */
 export namespace Hash {
 	/**
 	 * Generates a random hexadecimal hash string of the specified length.
