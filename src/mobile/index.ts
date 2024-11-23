4d6719b050d76421b6eb779f174215d4374f9212
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-11-23 15:57:18 +0000

    version bump

--- src/mobile/index.ts
+++ src/mobile/index.ts
@@ -1,6 +1,6 @@
 /**
  *
  * Exports functions usable in mobile contexts
- *
+ * @module
  */
 export * from "./hash.ts";
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,6 @@
 /**
  *
+ * @module
  * Exports functions usable in all contexts
  *
  */
